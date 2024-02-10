a551b0bd19f0b9fb74df3e2083838432134359ad
student <dhirajchavan355@gmail.com>
2024-02-10 19:40:56 +0530

    added databinding and apply

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -1,35 +1,38 @@
 package com.example.myapplication2;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
 
+import com.example.myapplication2.data.Bio;
 import com.example.myapplication2.databinding.ActivityMainBinding;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
+    private final Bio bio=new Bio();
 
-    private EditText enterhobbies;
-
-  private TextView hobbies;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        enterhobbies=findViewById(R.id.enter_hobies);
-        hobbies=findViewById(R.id.hobbies_text);
+
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+        bio.setName("Dhiraj chavan");
+        binding.setBio(bio);
+        binding.doneButton.setOnClickListener(this::addhobbies);
 
     }
 
     public void addhobbies(View view) {
-        hobbies.setText(String.format("hobbies :%s ",enterhobbies.getText().toString().trim()));
-        hobbies.setVisibility(View.VISIBLE);
+        bio.setHobbies(String.format("hobbies :%s ",binding.enterHobies.getText().toString().trim()));
+//        binding.hobbiesText.setText(String.format("hobbies :%s ",binding.enterHobies.getText().toString().trim()));
+        binding.invalidateAll();
+        binding.hobbiesText.setVisibility(View.VISIBLE);
 
         InputMethodManager inputMethodManager=(InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
         inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(),0);
