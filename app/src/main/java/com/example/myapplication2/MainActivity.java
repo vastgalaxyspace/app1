359b03e321b417c6ab2ce93b83e864b13390eccb
student <dhirajchavan355@gmail.com>
2024-02-10 15:56:59 +0530

    added databinding

--- app/src/main/java/com/example/myapplication2/MainActivity.java
+++ app/src/main/java/com/example/myapplication2/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.myapplication2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.example.myapplication2.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
+    private EditText enterhobbies;
+
+  private TextView hobbies;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        enterhobbies=findViewById(R.id.enter_hobies);
+        hobbies=findViewById(R.id.hobbies_text);
+
+    }
+
+    public void addhobbies(View view) {
+        hobbies.setText(String.format("hobbies :%s ",enterhobbies.getText().toString().trim()));
+        hobbies.setVisibility(View.VISIBLE);
+
+        InputMethodManager inputMethodManager=(InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(),0);
     }
 }
